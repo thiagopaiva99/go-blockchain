1c62158c28468204c289dcc33d79be7c2e05a427
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-05-16 18:02:18 -0300

    Adding the NewProofOfWork method

--- proofofwork.go
+++ proofofwork.go
@@ -16,3 +16,13 @@ type ProofOfWork struct {
 	block  *Block
 	target *big.Int
 }
+
+// NewProofOfWork builds and returns a ProofOfWork
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
