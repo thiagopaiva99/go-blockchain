0294b7230c9633d83a74c8a896a962f941279365
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-05-16 18:02:42 -0300

    Preparing the Data in the Proof Of Work

--- proofofwork.go
+++ proofofwork.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"math"
 	"math/big"
 )
@@ -25,4 +26,19 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 	pow := &ProofOfWork{b, target}
 
 	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
 }
